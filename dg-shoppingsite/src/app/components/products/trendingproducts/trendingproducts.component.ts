233938a3df830f77191e3deac6dbbc2a8d7b0ffd
Deepak George <Deepak.George.consultant@heart.org>
2023-03-20 21:37:18 +0000

    feat:trending products carousel added

--- dg-shoppingsite/src/app/components/products/trendingproducts/trendingproducts.component.ts
+++ dg-shoppingsite/src/app/components/products/trendingproducts/trendingproducts.component.ts
@@ -1,6 +1,8 @@
 import {  Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
 import { TrendingproductsService } from 'src/app/services/trendingproducts.service';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-trendingproducts',
@@ -10,12 +12,16 @@ import { TrendingproductsService } from 'src/app/services/trendingproducts.servi
 export class TrendingproductsComponent implements OnInit {
 
   trendingproducts: Product[]=[];
+  
 
-  constructor(private trendingProductService: TrendingproductsService)
-  {
 
-  }
+  constructor(private trendingProductService: TrendingproductsService,config: NgbCarouselConfig) {  
+    config.interval = 2000;  
+    config.wrap = true;  
+    config.keyboard = false;  
+    config.pauseOnHover = true;  
 
+  }  
   getTrendingProducts(): void{
     this.trendingProductService.getTrendingItems()
     .subscribe(data=>(this.trendingproducts=data));
@@ -27,6 +33,10 @@ export class TrendingproductsComponent implements OnInit {
   ngOnInit(): void {
     this.getTrendingProducts();
     console.log(this.trendingproducts);
+  }
+
+  ngAfterViewChecked(): void{
+ 
   }
 
 }
